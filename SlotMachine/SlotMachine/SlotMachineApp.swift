bb4e46ad8a1c2926da286483842663f50cac4071
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-03 18:45:38 +0530

    create a slot machine game

---
+++ SlotMachine/SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Gaurav Semwal on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
